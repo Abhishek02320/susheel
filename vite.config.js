ea1ba2c4f9f9bcfe94790aa0fb8bbf6e46283055
Abhishek02320 <113132228+Abhishek02320@users.noreply.github.com>
2024-06-03 10:06:17 +0530

    up

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/nightSky3d-Portfolio/",
+  server: {
+    mimeTypes: {
+      js: 'application/javascript'
+    }
+  }
 })
